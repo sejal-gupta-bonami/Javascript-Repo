c219caaac7dc4b62d289add6c4e436320102716e
Sejal <sejal.gupta@bonamisoftware.com>
2024-09-05 13:11:32 +0530

    map filter reduce

--- script2.js
+++ script2.js
@@ -45,20 +45,99 @@ console.log(p);
 
 //splice
 //remove
-let color = ['pink', 'white', 'green'];
+const color = ["pink", "white", "green"];
 let f = color.splice(1, 2);
 console.log(f); // returns deleted elements
 
 //add
-let color2 = ['pink', 'white', 'green'];
-color2.splice(2, 0,'black');
+let color2 = ["pink", "white", "green"];
+color2.splice(2, 0, "black");
 
 //replace
-let color3= ['pink', 'white', 'green'];
-color3.splice(2, 1, 'black');
+let color3 = ["pink", "white", "green"];
+color3.splice(2, 1, "black");
 console.log(color3);
 
-//negative index 
-let color4= ['pink', 'white', 'green'];
-color4.splice(-2, 1, 'black');
+//negative index
+let color4 = ["pink", "white", "green"];
+color4.splice(-2, 1, "black");
 console.log(color4);
+
+//slice method
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let ans = numbers.slice(2, 5); // makes a copy of deleted array
+console.log(numbers);
+console.log(ans);
+
+//some
+const arr5 = [1, 2, 3, 4, 5];
+let ans5 = arr5.some(function (x) {
+  return x % 2 === 0;
+});
+
+console.log(ans5);
+
+//map function
+
+function double(x) {
+  return x * 2;
+}
+
+let array1 = [2, 4, 6, 8, 10];
+let ans1 = array1.map(double);
+console.log(array1);
+console.log(ans1);
+
+//filter
+let output = array1.filter((x) => {
+  return;
+
+  x % 2 === 0;
+});
+console.log(output);
+
+//reduce method
+const inputArr = [2, 3, 4, 5, 6];
+let outputVal = inputArr.reduce(function (acc, curr) {
+  if (curr < acc) {
+    acc = curr;
+  }
+  return acc;
+}, 0);
+console.log(outputVal);
+
+//reduce practice question
+const person = [
+  { firstname: "Rahul", lastname: "gupta", age: 25 },
+  { firstname: "priya", lastname: "sharma", age: 50 },
+  { firstname: "dinesh", lastname: "garg", age: 25 },
+  { firstname: "shweta", lastname: "singh", age: 38 },
+];
+
+//print full name
+//use map
+var newArray = person.map(function (x) {
+  return x.firstname + " " + x.lastname;
+});
+
+console.log(newArray);
+
+//find number of persons according to age
+//use reduce
+//output - {25:2 ,50:1,38:1}
+var ansObj = person.reduce(function (acc, curr) {
+  if (acc[curr.age]) {
+    acc[curr.age] = acc[curr.age] + 1;
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(ansObj);
+
+//find first name of all the person whose age is less than 30
+
+let ansArray = person.filter((x) => x.age < 30).map((x) => x.firstname);
+
+console.log(ansArray);
